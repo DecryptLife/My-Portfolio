ac77568396bd82b3c6a68e732d987a5d81530ee8
DecryptLife <benson.thomas2602@gmail.com>
2023-10-16 23:33:40 -0500

    adding experience text

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,9 +7,15 @@ function About() {
             <h1 className=" about-text-header">Hi, I'm Benson.</h1>
             <b>
               <p className="about-text-detail">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-                laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-                Laborum, voluptas natus?
+                I am currently pursuing my Masters in Computer Science at Rice
+                University with a specialization in Data Science and Machine
+                Learning. I am passionate about FullStack Development and have
+                work experience as a Software Developer of around two years. I
+                love learning new technologies so you will find me learning some
+                new technology or programming language every now and then. I
+                spend a lot of time developing projects and practicing/learning
+                data structures and algorithms. When I am not coding or learning
+                something new you can find me at the gym!!
               </p>
             </b>
           </div>
--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -1,7 +1,34 @@
+import "./Experience.css";
+
 function Experience() {
   return (
     <section id="experience">
-      <h2>My Work Experience</h2>
+      <div className="container section-container">
+        <div className="section-header">
+          <h1 className="section-header-text">My Work Experience</h1>
+        </div>
+        <b>
+          <p>
+            With around two years of experience I have learned and have been
+            able to contribute which has helped me grow my knowledge and
+            skillset in the field of computer science. During Summer 23, I
+            worked as a Software Developer/ML Engineer Intern at Affekta LLC, a
+            startup that's focused on optimizing e-learning using Artificial
+            Intelligence. Before starting off my MCS at Rice University, I
+            worked as an Associate Software Developer for around a year where I
+            was exposed to the world of software development where I worked with
+            technologies like SpringBoot, MySQL, etc. These experiences have
+            been instrumental in shaping my career and fueling my passion for
+            technology
+          </p>
+        </b>
+
+        <div className="resume-container">
+          <div className="resume-wrapper">
+            <div className="resume-card"></div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
