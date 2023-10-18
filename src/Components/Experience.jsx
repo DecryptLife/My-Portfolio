7ff9e8016174d2e68094a84d1f31deeddd7927e1
DecryptLife <benson.thomas2602@gmail.com>
2023-10-17 23:20:19 -0500

    adding comps

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -28,11 +28,39 @@ function Experience() {
             <div className="resume-card">
               <div className="work-exp affekta">
                 <div className="work-exp-header">
-                  <h2>Affekta</h2>
+                  <div className="work-exp-icon">
+                    <img
+                      src="./affekta.jpeg"
+                      alt="affekta-icon"
+                      width={70}
+                      height={70}
+                    />
+                  </div>
+                  <div className="work-exp-title">
+                    <b>
+                      <h2>Affekta</h2>
+                    </b>
+                    <h6>Software Developer/ML Engineer Intern</h6>
+                  </div>
                 </div>
                 <div className="work-exp-body">
-                  <ul>
-                    <li>Worked as </li>
+                  <ul className="custom-bullet">
+                    <li>
+                      Improved the frontend code for the startup’s platform,
+                      creating and enhancing web pages and components using
+                      React, resulting in approximately 30% improvement in user
+                      page load times
+                    </li>
+                    <li>
+                      Conducted research and utilized AI APIs to develop a
+                      pipeline, enhancing the platform’s capabilities thereby
+                      producing a 20% increase in user engagement
+                    </li>
+                    <li>
+                      Gathered valuable feedback and insights through customer
+                      interactions which was leveraged to make modifications to
+                      the website which reduced the bounce rates by 25%
+                    </li>
                   </ul>
                 </div>
               </div>
@@ -40,9 +68,42 @@ function Experience() {
               <br />
               <div className="work-exp virtusa">
                 <div className="work-exp-header">
-                  <h2>Virtusa</h2>
+                  <div className="work-exp-icon">
+                    <img
+                      src="./virtusa.jpeg"
+                      alt="virtusa icon"
+                      width={70}
+                      height={70}
+                    />
+                  </div>
+                  <div className="work-exp-title">
+                    <b>
+                      <h2>Virtusa</h2>
+                    </b>
+                    <h6>Associate Software Engineer</h6>
+                  </div>
                 </div>
-                <div className="work-exp-body"></div>
+                <div className="work-exp-body">
+                  <ul className="custom-bullet">
+                    <li>
+                      Played an integral role in the development of 3 Spring
+                      Boot projects, demonstrating expertise in building robust
+                      and high-performance applications achieving more than 90%
+                      team satisfaction{" "}
+                    </li>
+                    <li>
+                      Maintained a consistent position as the top performer
+                      within the group, constantly achieving above 85% code
+                      quality scores and earned responsibility for new and
+                      challenging projects
+                    </li>
+                    <li>
+                      Mentored and coached 3 junior developers in mastering
+                      Spring Boot, accelerating the onboarding process and
+                      enhancing team productivity by approximately 50%
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
