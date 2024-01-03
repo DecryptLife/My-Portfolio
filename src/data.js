62da7f68619955b9ca41ebdbb24c41c05d1635d6
DecryptLife <benson.thomas2602@gmail.com>
2024-01-02 21:20:00 -0600

    updated data.js

--- src/data.js
+++ src/data.js
@@ -13,7 +13,7 @@ export const projects = [
     description:
       "ReLearn is an e-learning platform that has two user-access: admin and user and each user-access provided with different functionalities. Initially created using MERN stack, re-based the code to use TypeScript and currently in the transition to NextJS",
     image: "./relearn.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://github.com/DecryptLife/ReLearn",
   },
   {
     title: "Fine Tuning Text-to-Image Diffusion Models with Style Transfer",
